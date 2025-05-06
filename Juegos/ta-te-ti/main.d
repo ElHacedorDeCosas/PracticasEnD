9f175c6fd85d149a67ae77f6f56c8aaa7f0f2e7f
ElHacedorDeCosas <elultimocreador@gmail.com>
2025-05-06 12:22:14 -0300

    Terminé ta-te-ti

--- Juegos/ta-te-ti/main.d
+++ Juegos/ta-te-ti/main.d
@@ -15,7 +15,7 @@ void turno_jugador(char[9] *ref_tab, char *ref_jug, char *ref_com, bool *ref_tur
     writeln("Por favor ingrese la pocición en la que quiere colocar su ficha (1-9)");
     auto entrada_txt = readln();
 
-    try {
+    if (isNumeric(chomp(entrada_txt)) == true) {
         int entrada_int = to!int(chomp(entrada_txt));
         if ((entrada_int - 1) >= 0 && (entrada_int - 1) < 9) {
             if ((*ref_tab)[entrada_int - 1] != *ref_jug && (*ref_tab)[entrada_int - 1] != *ref_com) {
@@ -25,12 +25,9 @@ void turno_jugador(char[9] *ref_tab, char *ref_jug, char *ref_com, bool *ref_tur
                 writeln("Esa casilla ya está ocupada");
             }
         }else {
-                writeln("El numero ingresado está por fuera de las casillas del tablero");
+            writeln("El numero ingresado está por fuera de las casillas del tablero");
         }
-    }catch (Exception err){
-        if (DEBUG_MODE == true){
-            writeln("Error al leer la entrada: ", err);
-        }
+    }else {
         writeln("Vuelva a ingresar su opción");
     }
 }
@@ -45,13 +42,40 @@ void turno_computador(char[9] *ref_tab, char *ref_jug, char *ref_com, bool *ref_
     }
 }
 
-string es_victoria(char[9] *ref_tab, char ref_jug, char ref_com, bool *ref_victoria){
+string verificar_victoria(char[9] *ref_tab, char *ref_jug, char *ref_com, bool *ref_victoria){
     string salida = "Error";
     int no_espacio = 0;
 
-    if (){
-        salida = "VICTORIA DEL JUGADOR, FELICIDADES!";
+    if ((*ref_tab)[0] == *ref_jug && (*ref_tab)[1] == *ref_jug && (*ref_tab)[2] == *ref_jug || //Primera Linea
+        (*ref_tab)[3] == *ref_jug && (*ref_tab)[4] == *ref_jug && (*ref_tab)[5] == *ref_jug || //Segunda Linea
+        (*ref_tab)[6] == *ref_jug && (*ref_tab)[7] == *ref_jug && (*ref_tab)[8] == *ref_jug || //Tercera Linea
+        (*ref_tab)[0] == *ref_jug && (*ref_tab)[4] == *ref_jug && (*ref_tab)[8] == *ref_jug || //Diagonal Derecha
+        (*ref_tab)[2] == *ref_jug && (*ref_tab)[4] == *ref_jug && (*ref_tab)[6] == *ref_jug || //Diagonal Izquierda
+        (*ref_tab)[0] == *ref_jug && (*ref_tab)[3] == *ref_jug && (*ref_tab)[6] == *ref_jug || //Primera Columna
+        (*ref_tab)[1] == *ref_jug && (*ref_tab)[4] == *ref_jug && (*ref_tab)[7] == *ref_jug || //Segunda Columna
+        (*ref_tab)[2] == *ref_jug && (*ref_tab)[5] == *ref_jug && (*ref_tab)[8] == *ref_jug ){ //Tercera Columna
+        salida = "¡VICTORIA DEL JUGADOR, FELICIDADES!";
         *ref_victoria = true;
+    }else if ((*ref_tab)[0] == *ref_com && (*ref_tab)[1] == *ref_com && (*ref_tab)[2] == *ref_com || //Primera Linea
+              (*ref_tab)[3] == *ref_com && (*ref_tab)[4] == *ref_com && (*ref_tab)[5] == *ref_com || //Segunda Linea
+              (*ref_tab)[6] == *ref_com && (*ref_tab)[7] == *ref_com && (*ref_tab)[8] == *ref_com || //Tercera Linea
+              (*ref_tab)[0] == *ref_com && (*ref_tab)[4] == *ref_com && (*ref_tab)[8] == *ref_com || //Diagonal Derecha
+              (*ref_tab)[2] == *ref_com && (*ref_tab)[4] == *ref_com && (*ref_tab)[6] == *ref_com || //Diagonal Izquierda
+              (*ref_tab)[0] == *ref_com && (*ref_tab)[3] == *ref_com && (*ref_tab)[6] == *ref_com || //Primera Columna
+              (*ref_tab)[1] == *ref_com && (*ref_tab)[4] == *ref_com && (*ref_tab)[7] == *ref_com || //Segunda Columna
+              (*ref_tab)[2] == *ref_com && (*ref_tab)[5] == *ref_com && (*ref_tab)[8] == *ref_com ){ //Tercera Columna
+        salida = "¡VICTORIA DEL COMPUTADOR, MEJOR SUERTE PARA LA PROXIMA!";
+        *ref_victoria = true;
+    }else {
+        foreach(caracter; *ref_tab) {
+            if (caracter != ' ') {
+                    no_espacio++;
+                }
+        }
+        if (no_espacio == 9) {
+            salida = "¡EMPATE!";
+            *ref_victoria = true;
+        }
     }
     no_espacio = 0;
     return salida;
@@ -74,9 +98,13 @@ void main(){
         while (es_turno_jugador == true) {
             turno_jugador(&tablero, &jugador, &computador, &es_turno_jugador);
         }
+        ganador = verificar_victoria(&tablero, &jugador, &computador, &es_victoria);
         es_turno_computador = true;
         while (es_turno_computador == true) {
             turno_computador(&tablero, &jugador, &computador, &es_turno_computador);
         }
+        ganador = verificar_victoria(&tablero, &jugador, &computador, &es_victoria);
     }
+    msj(&tablero, &jugador, &computador);
+    writeln(ganador);
 }
