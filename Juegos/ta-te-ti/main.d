5eccd2abeb9c54077758c18985382f4656c91828
ElHacedorDeCosas <elultimocreador@gmail.com>
2025-05-07 15:03:02 -0300

    Ajustes a ta-te-ti

--- Juegos/ta-te-ti/main.d
+++ Juegos/ta-te-ti/main.d
@@ -1,7 +1,5 @@
 import std.stdio, std.conv, std.string, std.exception, std.random;
 
-const bool DEBUG_MODE = false;
-
 void msj(char[9] *ref_tab, char *ref_jug, char *ref_com) {
     writefln("  %s  |  %s  |  %s  ", (*ref_tab)[0], (*ref_tab)[1], (*ref_tab)[2]);
     writefln("-----|-----|-----");
@@ -11,10 +9,11 @@ void msj(char[9] *ref_tab, char *ref_jug, char *ref_com) {
 }
 
 void turno_jugador(char[9] *ref_tab, char *ref_jug, char *ref_com, bool *ref_turno_jug){
-
+    // - Entrada por consola - //
     writeln("Por favor ingrese la pocición en la que quiere colocar su ficha (1-9)");
     auto entrada_txt = readln();
 
+    // - Manejo de la entrada - //
     if (isNumeric(chomp(entrada_txt)) == true) {
         int entrada_int = to!int(chomp(entrada_txt));
         if ((entrada_int - 1) >= 0 && (entrada_int - 1) < 9) {
@@ -34,7 +33,7 @@ void turno_jugador(char[9] *ref_tab, char *ref_jug, char *ref_com, bool *ref_tur
 
 void turno_computador(char[9] *ref_tab, char *ref_jug, char *ref_com, bool *ref_turno_com){
 
-    auto num_rand = uniform(1, 10);
+    auto num_rand = uniform(1, 10);//1..9
     if ((*ref_tab)[num_rand - 1] != *ref_jug && (*ref_tab)[num_rand - 1] != *ref_com) {
         (*ref_tab)[num_rand - 1] = *ref_com;
         *ref_turno_com = false;
@@ -46,6 +45,7 @@ string verificar_victoria(char[9] *ref_tab, char *ref_jug, char *ref_com, bool *
     string salida = "Error";
     int no_espacio = 0;
 
+    // - Verificación de victoria del Jugador/Computador o Empate - //
     if ((*ref_tab)[0] == *ref_jug && (*ref_tab)[1] == *ref_jug && (*ref_tab)[2] == *ref_jug || //Primera Linea
         (*ref_tab)[3] == *ref_jug && (*ref_tab)[4] == *ref_jug && (*ref_tab)[5] == *ref_jug || //Segunda Linea
         (*ref_tab)[6] == *ref_jug && (*ref_tab)[7] == *ref_jug && (*ref_tab)[8] == *ref_jug || //Tercera Linea
